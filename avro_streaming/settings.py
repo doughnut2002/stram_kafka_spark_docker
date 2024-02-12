05262b3104218527514a36de4cfe68046a78998b
doughnut2002 <shekhar888700@gmail.com>
2024-02-12 14:40:08 +0530

    Done

--- avro_streaming/settings.py
+++ avro_streaming/settings.py
@@ -1,7 +1,7 @@
-INPUT_DATA_PATH = '../resources/rides.csv'
+INPUT_DATA_PATH = './resources/rides.csv'
 
-RIDE_KEY_SCHEMA_PATH = '../resources/schemas/taxi_ride_key.avsc'
-RIDE_VALUE_SCHEMA_PATH = '../resources/schemas/taxi_ride_value.avsc'
+RIDE_KEY_SCHEMA_PATH = '../resources/schema/taxi_ride_key.avsc'
+RIDE_VALUE_SCHEMA_PATH = '../resources/schema/taxi_ride_value.avsc'
 
 SCHEMA_REGISTRY_URL = 'http://localhost:8081'
 BOOTSTRAP_SERVERS = 'localhost:9092'
